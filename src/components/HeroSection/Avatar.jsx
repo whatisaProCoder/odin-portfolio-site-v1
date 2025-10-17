2158d7c29e7828159cacedb0a97b6253b399935c
whatisaProCoder <debnathpritam2005@gmail.com>
2025-10-18 02:00:48 +0530

    fix

--- src/components/HeroSection/Avatar.jsx
+++ src/components/HeroSection/Avatar.jsx
@@ -3,7 +3,7 @@ function Avatar({ src, style }) {
     <img
       src={src}
       style={{ height: style.width, width: style.width, zIndex: style.zIndex }}
-      className="rounded-[1rem] border-1 border-[rgba(50,50,50,60%)] drop-shadow-[2px_4px_8px_rgba(0,0,0,25%)]"
+      className="rounded-[1rem] border-1 border-[rgba(50,50,50,60%)] drop-shadow-[2px_4px_8px_rgba(0,0,0,25%)] sm:transition-transform sm:hover:scale-102"
     ></img>
   );
 }
