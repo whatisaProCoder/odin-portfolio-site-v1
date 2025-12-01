e26ab95c0f880a7c1f23bbcfca18da74ad6ff0cc
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-01 20:21:35 +0530

    unloading the loading screen using useState

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,11 +3,18 @@ import TechStackSection from "../components/TechStackSection";
 import WorkSection from "../components/WorkSection";
 import Footer from "../components/Footer";
 import LoadingScreen from "../components/common/LoadingScreen";
+import { useState } from "react";
 
 function MainPage() {
+  const [loading, setLoading] = useState(true);
+
+  setInterval(() => {
+    setLoading(false);
+  }, 1800);
+
   return (
     <>
-      <LoadingScreen />
+      {loading && <LoadingScreen />}
       <div className="m-10 mt-16 max-sm:m-4 max-sm:mt-16">
         <HeroSection />
         <WorkSection />
