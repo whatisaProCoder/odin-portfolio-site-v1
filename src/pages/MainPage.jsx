d80b61c0f5233e98397c470e33036c26a739ce84
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-17 01:35:19 +0530

    fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 function MainPage() {
   const [loading, setLoading] = useState(true);
 
-  setInterval(() => {
+  setTimeout(() => {
     setLoading(false);
   }, 1800);
 
