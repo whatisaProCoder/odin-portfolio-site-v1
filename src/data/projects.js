f8468e88b7ebaf39f0ed462c132345b4c2f4c0cd
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-05 00:21:51 +0530

    add new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,15 @@ import { LogoSet } from "../constants/logoSet";
 export const Projects = [
   {
     id: 0,
+    title: "Fakestore - E-Commerce Site",
+    imageSrc: "https://github.com/whatisaProCoder/odin-fakestore-site/blob/main/public/showcase.png?raw=true",
+    summary: "An e-commerce web application built as part of The Odin Project's Shopping Cart assignment. Evolved into a comprehensive mock online store",
+    repository: "https://github.com/whatisaProCoder/odin-fakestore-site",
+    live: "https://odin-fakestore-site.vercel.app/",
+    techUsed: [LogoSet.React, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.vitejs]
+  },
+  {
+    id: 1,
     title: "CV Generator",
     imageSrc: "https://github.com/whatisaProCoder/odin-cv-generator-app/blob/main/src/assets/images/showcase.png?raw=true",
     summary: "Lets you create professional CVs with real-time preview. You fill out the forms on the left, and your CV updates instantly on the right.",
@@ -11,7 +20,7 @@ export const Projects = [
     techUsed: [LogoSet.React, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.vitejs]
   },
   {
-    id: 1,
+    id: 2,
     title: "AoT - Memory Game",
     imageSrc: "https://github.com/whatisaProCoder/aot-memory-game/blob/main/public/showcase.png?raw=true",
     summary: "A memory card game featuring AoT characters with three difficulty levels, where players must avoid clicking the same character twice.",
@@ -20,7 +29,7 @@ export const Projects = [
     techUsed: [LogoSet.React, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.vitejs]
   },
   {
-    id: 2,
+    id: 3,
     title: "Portfolio Website",
     imageSrc: "./showcase.png",
     summary: "My first dedicated portfolio website made in React, showcasing my interests, projects and tech stack in a custom-made creative Figma design.",
@@ -29,7 +38,7 @@ export const Projects = [
     techUsed: [LogoSet.React, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.vitejs]
   },
   {
-    id: 3,
+    id: 4,
     title: "Polymath",
     imageSrc: "https://github.com/whatisaProCoder/odin-task-management-app/blob/main/demo/app_preview.png?raw=true",
     summary: "An intuitive task management application built with just VanillaJS, no frameworks and packaged as a desktop application using Electron.",
@@ -38,7 +47,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.JavaScript, LogoSet.Webpack, LogoSet.Electron]
   },
   {
-    id: 4,
+    id: 5,
     title: "Weather App",
     imageSrc: "https://github.com/whatisaProCoder/odin-weather-app/blob/main/demo/dashboard.png?raw=true",
     summary: "A sleek, responsive weather application that provides current weather data and weekly forecasts for any location around the world.",
@@ -47,7 +56,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.Webpack]
   },
   {
-    id: 5,
+    id: 6,
     title: "Giphy App",
     imageSrc: "https://github.com/whatisaProCoder/odin-giphy-api-ui/raw/main/src/images/demo.gif",
     summary: "A simple application that lets you search for GIFs using the GIPHY API. It demonstrates how to fetch data from an external API.",
@@ -56,7 +65,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.Webpack]
   },
   {
-    id: 6,
+    id: 7,
     title: "Custom Image Carousel",
     imageSrc: "https://github.com/whatisaProCoder/custom-image-carousel-component/blob/main/demo/demo.png?raw=true",
     summary: "A lightweight, easy-to-use npm package that provides image carousel component with navigation dots, arrow controls, and smooth scrolling.",
@@ -65,7 +74,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.Webpack, LogoSet.npm]
   },
   {
-    id: 7,
+    id: 8,
     title: "Custom Dropdown Menu",
     imageSrc: "https://github.com/whatisaProCoder/custom-dropdown-menu/blob/main/demo/recording.gif?raw=true",
     summary: "A beginner-friendly, lightweight npm package that provides dropdown menu component that's easy to use and customize.",
@@ -74,7 +83,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.TailwindCSS, LogoSet.JavaScript, LogoSet.Webpack, LogoSet.npm]
   },
   {
-    id: 8,
+    id: 9,
     title: "Chillax Restaurant Website",
     imageSrc: "https://github.com/whatisaProCoder/odin-restaurant-site/blob/main/demo/showcase.png?raw=true",
     summary: "A modern restaurant website built as single-page application showcases a fictional restaurant with dynamic content loading.",
@@ -83,7 +92,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.JavaScript, LogoSet.Webpack]
   },
   {
-    id: 9,
+    id: 10,
     title: "Tic Tac Toe",
     imageSrc: "https://github.com/whatisaProCoder/odin-tic-tac-toe-game/blob/main/demo/showcase.png?raw=true",
     summary: "A simple web-based Tic-Tac-Toe game built with HTML, CSS, and JavaScript as part of The Odin Project curriculum.",
@@ -92,7 +101,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.JavaScript]
   },
   {
-    id: 10,
+    id: 11,
     title: "Library Application",
     imageSrc: "https://github.com/whatisaProCoder/odin-library-app/blob/main/demo/showcase.png?raw=true",
     summary: "A simple, responsive library web application that allows users to add, view, and manage books in a sleek, minimalist interface.",
@@ -101,7 +110,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.JavaScript]
   },
   {
-    id: 11,
+    id: 12,
     title: "Admin Dashboard",
     imageSrc: "https://github.com/whatisaProCoder/odin-admin-dashboard/blob/main/demo/showcase.png?raw=true",
     summary: "Admin Dashboard for The Odin Project — Flexbox & Grid layout, minimalist design, light/dark toggle.",
@@ -110,7 +119,7 @@ export const Projects = [
     techUsed: [LogoSet.HTML, LogoSet.CSS, LogoSet.JavaScript]
   },
   {
-    id: 12,
+    id: 13,
     title: "TOP - Sign Up Form",
     imageSrc: "https://github.com/whatisaProCoder/odin-signup-form/blob/main/demo/showcase.png?raw=true",
     summary: "This is the basic Sign-Up Form we get to build in the \"Intermediate HTML & CSS\" section of The Odin Project.",
