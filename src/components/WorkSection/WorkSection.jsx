453332f5435a84c613ad33012c685ead951010c1
whatisaProCoder <debnathpritam2005@gmail.com>
2026-03-21 19:28:33 +0530

    fix

--- src/components/WorkSection/WorkSection.jsx
+++ src/components/WorkSection/WorkSection.jsx
@@ -4,7 +4,7 @@ import SectionHeader from "../common/SectionHeader";
 import WorkCard from "./WorkCard";
 
 function WorkSection() {
-  const [uptilID, setUptilID] = useState(9);
+  const [uptilID, setUptilID] = useState(8);
 
   const handleShowMore = () => {
     setUptilID(Projects.length - 1);
